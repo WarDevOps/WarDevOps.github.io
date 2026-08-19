7c22a1bc702baeba4051ca4b48215dc69084e4e4
WarDevOps <martiallow485@gmail.com>
2026-08-19 17:23:03 +0900

    Revert "Turn off visitorStatistics"
    This reverts commit 2f258ed31f5d704a6eff8866a91a3a595fec7b52.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
     import { MARKER_LAYOUT_VERSION, loadMarkerLayout, saveMarkerLayout as saveMarkerLayoutToStorage } from './marker-storage.js';
-    import { initDiscordMemberCount } from './discord-stats.js';
+import { initVisitorCounter } from './visitor-counter.js';
+import { initDiscordMemberCount } from './discord-stats.js';
     import { commentImages } from './comment-images.js?v=comment-images-20260817';
     import { maps, translations } from './data.js?v=new-maps-20260818';
 
@@ -1380,4 +1381,5 @@
     });
     setLanguage("en");
     restoreFromUrl();
+    initVisitorCounter();
     initDiscordMemberCount();
