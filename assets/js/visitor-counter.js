52042ace4243097033620d23ea73c9dcf7345c99
WarDevOps <martiallow485@gmail.com>
2026-08-19 17:06:35 +0900

    Delete visitor-counter.js

--- assets/js/visitor-counter.js
+++
@@ -1,7 +0,0 @@
-const VISITOR_COUNTER_ENDPOINT = 'https://visitor.6developer.com/visit';
-const SITE_TIMEZONE = 'Asia/Seoul';
-
-function setCount(element, value) {
-  if (!Number.isFinite(value) || value < 0) return;
-  element.textContent = new Intl.NumberFormat(document.documentElement.lang || 'en').format(value);
-}
